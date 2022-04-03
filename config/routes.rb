6a2ad235f23ae9528e88407ffccc07293e706b3c
shinjijp <hi.cheese0707@gmail.com>
2022-04-03 12:57:05 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
- resources :users, only: [:show, :edit]
+ resources :users, only: [:show, :edit, :update]
  devise_for :users
- resources :post_images, only: [:new, :create, :index, :show, :destroy]
+ resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+   resource :favorites, only: [:create, :destroy]
+   resources :post_comments, only: [:create, :destroy]
+ end
  root to: "homes#top"
 
  get "/homes/about" => "homes#about", as: "about"
